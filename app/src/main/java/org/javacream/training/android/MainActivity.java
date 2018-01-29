32e437e67a724270aacac6a98d699785dfb18d64
Rainer <training@rainer-sawitzki.de>
2018-01-29 16:18:43 +0100

    implement simple event handling

--- app/src/main/java/org/javacream/training/android/MainActivity.java
+++ app/src/main/java/org/javacream/training/android/MainActivity.java
@@ -8,13 +8,39 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText lastnameInput;
+    private EditText firstnameInput;
+    private EditText genderInput;
+    private EditText heightInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.people_layout);
+        lastnameInput = findViewById(R.id.lastnameInput);
+        firstnameInput = findViewById(R.id.firstnameInput);
+        genderInput = findViewById(R.id.genderInput);
+        heightInput = findViewById(R.id.heightInput);
     }
 
+
+    public void handleSave(View view) {
+        String lastname = lastnameInput.getText().toString();
+        String firstname = firstnameInput.getText().toString();
+        String gender = genderInput.getText().toString();
+        String height = heightInput.getText().toString();
+        String message = "saved person, lastname=" + lastname + ", firstname=" + firstname + ", gender=" + gender + ", height=" + height;
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+    public void handleReset(View view) {
+        lastnameInput.setText("");
+        firstnameInput.setText("");
+        genderInput.setText("");
+        heightInput.setText("");
+    }
 }
