835a2730b0b7ec8903dbaba9c74f24b619eac359
Rainer <training@rainer-sawitzki.de>
2018-02-01 10:54:48 +0100

    implement webview

--- app/src/main/java/org/javacream/training/android/MainActivity.java
+++ app/src/main/java/org/javacream/training/android/MainActivity.java
@@ -1,5 +1,7 @@
 package org.javacream.training.android;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,13 +10,39 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private WebView webView;
+    private EditText uriInput;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        uriInput = findViewById(R.id.urlInput);
+        webView = findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
     }
+
+    public void handleLoadUri(View view) {
+
+        webView.loadUrl(uriInput.getText().toString());
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return false;
+        }
+    }
+
+
+
 
 }
