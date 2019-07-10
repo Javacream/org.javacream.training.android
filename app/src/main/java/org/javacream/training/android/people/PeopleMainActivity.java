3ec27690928bc5908b3eec0f7ff00b74f6ac0656
Rainer Sawitzki <training@rainer-sawitzki.de>
2019-07-10 16:50:52 +0200

    implement changing contentLayout

--- app/src/main/java/org/javacream/training/android/people/PeopleMainActivity.java
+++ app/src/main/java/org/javacream/training/android/people/PeopleMainActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class PeopleMainActivity extends AppCompatActivity {
 
@@ -13,6 +16,8 @@ public class PeopleMainActivity extends AppCompatActivity {
     private EditText firstnameInput;
     private EditText genderInput;
     private EditText heightInput;
+    private LinearLayout logView;
+    private Button saveButton;
 
     {
         //DON'T DO ANYTHING INSIDE AN ACTIVITIES CONSTRUCTOR
@@ -25,15 +30,20 @@ public class PeopleMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         Log.i("Lifecycle", "onCreate");
         super.onCreate(savedInstanceState);
-        EditText lastnameInputNull  = this.findViewById(R.id.lastnameInput);
         setContentView(R.layout.person_input);
+        logView = (LinearLayout) getLayoutInflater().inflate(R.layout.log_layout, null);
+        logView.setOrientation(LinearLayout.VERTICAL);
+        createFields();
+    }
+
+    private void createFields(){
         lastnameInput  = this.findViewById(R.id.lastnameInput);
         firstnameInput  = this.findViewById(R.id.firstnameInput);
         genderInput  = this.findViewById(R.id.genderInput);
         heightInput  = this.findViewById(R.id.heightInput);
         lastnameInput.setText("HELLO FROM ACTIVITY");
+        saveButton = findViewById(R.id.saveButton);
     }
-
     @Override
     protected void onStart() {
         super.onStart();
@@ -59,7 +69,14 @@ public class PeopleMainActivity extends AppCompatActivity {
         String firstname = firstnameInput.getText().toString();
         String gender = genderInput.getText().toString();
         String height = heightInput.getText().toString();
-        Log.i("Action", "saving lastname=" + lastname + ", firstname=" + firstname + ", height=" + height + ", gender=" + gender);
+        String message = "saving lastname=" + lastname + ", firstname=" + firstname + ", height=" + height + ", gender=" + gender;
+        Log.i("Action", message);
+        TextView textView = new TextView(this);
+        textView.setText(message);
+        textView.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT));
+        logView.addView(textView);
     }
     public void doClear(View view){
         //Beliebige Code-Sequenz
@@ -68,5 +85,12 @@ public class PeopleMainActivity extends AppCompatActivity {
         firstnameInput.setText("");
         genderInput.setText("");
         heightInput.setText("");
+    }
+    public void doShowLog(View view){
+        setContentView(logView);
+    }
+    public void doShowPersonInput(View view){
+        setContentView(R.layout.person_input);
+        createFields();
     }
 }
