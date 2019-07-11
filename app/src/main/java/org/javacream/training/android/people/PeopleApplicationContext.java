1dfcdc64ead6158bf945dc8964bf40dbba26accc
Rainer Sawitzki <training@rainer-sawitzki.de>
2019-07-11 14:23:07 +0200

    implement create person controller

--- app/src/main/java/org/javacream/training/android/people/PeopleApplicationContext.java
+++ app/src/main/java/org/javacream/training/android/people/PeopleApplicationContext.java
@@ -1,5 +1,6 @@
 package org.javacream.training.android.people;
 
+import org.javacream.training.android.people.controller.CreatePersonController;
 import org.javacream.training.android.people.model.MapPeopleModel;
 import org.javacream.training.android.people.model.PeopleModel;
 
@@ -7,8 +8,19 @@ public class PeopleApplicationContext {
 
     private static MapPeopleModel peopleModel;
 
+    public static CreatePersonController createPersonController() {
+        return createPersonController;
+    }
+
+    private static CreatePersonController createPersonController;
+
     static{
+        //Objekte parameterlos erzeugen
         peopleModel = new MapPeopleModel();
+        createPersonController = new CreatePersonController();
+
+        //Dependencies injecten = setzen
+        createPersonController.setPeopleModel(peopleModel);
     }
 
     public static PeopleModel peopleModel(){
